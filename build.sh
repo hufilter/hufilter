3476c369edbe6b9a41a8f92e4a0803df005d01a6
Peter <szpeter80@users.noreply.github.com>
2015-01-30 12:41:16 +0100

    +First scripted build

--- build.sh
+++ build.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+BUILDFN="./hufilter.tmp"
+
+echo -e > $BUILDFN;
+for x in ` ls -1 ./build/ `;
+    do
+        echo "!##################### $x ##################### " >> $BUILDFN;
+        cat "./build/$x" >> "$BUILDFN";
+        echo "!##################### END $x ##################### " >> $BUILDFN;
+    done;
+
+HFVER=`date "+%Y%m%d%H%M"`;
+sed s/#VERSION#/$HFVER/g < $BUILDFN >hufilter.new
+mv hufilter.new hufilter.txt
+rm -f $BUILDFN
