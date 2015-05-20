73d1b9187826457e3b46e70c04e5ef385df84eaf
Peter <szpeter80@users.noreply.github.com>
2015-05-20 17:00:52 +0200

    + Added new version of filter based on user submission, closes issue#1

--- build.sh
+++ build.sh
@@ -14,3 +14,7 @@ HFVER=`date "+%Y%m%d%H%M"`;
 sed s/#VERSION#/$HFVER/g < $BUILDFN >hufilter.new
 mv hufilter.new hufilter.txt
 rm -f $BUILDFN
+
+wget https://easylist-downloads.adblockplus.org/easylist.txt
+diff hufilter.txt easylist.txt --new-line-format="" --old-line-format="%L" --unchanged-line-format="" > hufilter-minuseasylist.txt
+rm -f ./easylist.txt
