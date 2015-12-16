3ae926a42a7208fc4838c061aa167b57c32c97e0
Peter <szpeter80@users.noreply.github.com>
2015-12-16 16:05:22 +0100

    + Licence tag added (copyleft) ~ new email - deleted gemius full domain blocking due to issue#8 ~ easylist minused compilation is broken, probably ISP issues, disabled for now

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ sed s/#VERSION#/$HFVER/g < $BUILDFN >hufilter.new
 mv hufilter.new hufilter.txt
 rm -f $BUILDFN
 
-wget https://easylist-downloads.adblockplus.org/easylist.txt
-diff hufilter.txt easylist.txt --new-line-format="" --old-line-format="%L" --unchanged-line-format="" > hufilter-minuseasylist.txt
-rm -f ./easylist.txt
+# Easylist is not reachable atm from DIGI HU networks... disabled temporarily
+#wget https://easylist-downloads.adblockplus.org/easylist.txt
+#diff hufilter.txt easylist.txt --new-line-format="" --old-line-format="%L" --unchanged-line-format="" > hufilter-minuseasylist.txt
+#rm -f ./easylist.txt
